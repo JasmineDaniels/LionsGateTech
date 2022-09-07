392ce60e2a17ad2d23bdbb2aa83597c4f71c92fc
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-09-06 21:32:10 -0400

    updated client side validation

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,26 +1,37 @@
 console.log(`I am listening`)
 
+async function validatePassword(username, password){
+
+    if (password.length < 10){
+        alert(`Password must be 10 characters or more..`)
+    } else {
+        const awaitFetch = await fetch(`api/users/signup`, {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+    
+        if (awaitFetch.ok){
+            alert(`Welcome to TechHub ${username}! \n Please Sign In..`)
+            document.location.replace(`/login`) 
+        } else {
+            alert(`Failed to signup`)
+        }
+    }
+    
+}
+
 document.querySelector('#register').addEventListener('click', async (event) => {
     event.preventDefault()
 
     const username = document.getElementsByTagName('input')[0].value;
     const password = document.getElementsByTagName('input')[1].value;
 
-    const awaitFetch = await fetch(`api/users/signup`, {
-        method: 'POST',
-        body: JSON.stringify({
-            username,
-            password,
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-
-    if (awaitFetch.ok){
-        alert(`Welcome to TechHub ${username}! \n Please Sign In..`)
-        document.location.replace(`/login`) 
-    } else {
-        alert(`Failed to signup`)
-    }
-})
+    const checkPasswordLength = await validatePassword(username, password)
+    
+});
